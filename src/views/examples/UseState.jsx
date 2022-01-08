febd00c1cb492cab9f4144f25ed5002962e472ba
laianribas <laian67a@gmail.com>
2022-01-08 16:20:11 -0300

    Ajuste layout + primeiro useState

---
+++ src/views/examples/UseState.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseState = props => {
+  const [num, setNum] = useState(0)
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <span className="text">{num}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => {
+              setNum(0)
+            }}
+          >
+            0
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setNum(num - 1)
+            }}
+          >
+            -1
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setNum(num + 1)
+            }}
+          >
+            +1
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setNum(1000)
+            }}
+          >
+            1000
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setNum(current => current + 1000)
+            }}
+          >
+            +1000
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício #02" />
+    </div>
+  )
+}
+
+export default UseState

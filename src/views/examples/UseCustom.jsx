58b894061c16adda55bc73d9e47cd302912f9b0c
laianribas <laian67a@gmail.com>
2022-01-11 10:41:52 -0300

    Criando Hook personalizado para encapsular fetch

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+import { useCounter } from '../../hooks/useCounter'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useFetch } from '../../hooks/useFetch'
 const UseRef = props => {
+  const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+  const response = useFetch(url)
+
+  function showStates(state) {
+    return state.map(state => (
+      <li key={state.nome}>
+        {state.nome} - {state.sigla}
+      </li>
+    ))
+  }
+
+  const [count, inc, dec] = useCounter()
   return (
     <div className="UseCustom">
       <PageTitle
         title="Seu Hook"
         subtitle="Vamos aprender como criar o nosso próprio Hook!"
       />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => inc()}>
+            Inc
+          </button>
+          <button className="btn" onClick={() => dec()}>
+            dec
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <ul>{!response.loading ? showStates(response.data) : false}</ul>
+      </div>
     </div>
   )
 }
